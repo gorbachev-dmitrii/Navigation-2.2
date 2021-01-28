31a37e8580292d8cda0f3824a90749fc5fd9d1f2
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2021-01-28 23:50:13 +0300

    init commit for TVHW1

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Dima Gorbachev on 28.01.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        
+        //navigationBar.isHidden = true
+    }
+}
