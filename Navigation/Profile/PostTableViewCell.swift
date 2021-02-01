7a367800d98c04633dd818487162a9d0a10418d9
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2021-02-02 00:56:49 +0300

    init commit for tableViewHW2

---
+++ Navigation/Profile/PostTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+//  Created by Dima Gorbachev on 02.02.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import Foundation
+
+class PostTableViewCell: UITableViewCell {
+    
+}
