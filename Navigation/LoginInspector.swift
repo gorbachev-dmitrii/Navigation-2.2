0818bd9306a60636ad07a4b934a8eb888e8773de
Dima Gorbachev <dimagorbachev@192.168.1.4>
2022-03-03 00:28:36 +0300

    success in creating user in LoginInspector

--- Navigation/LoginInspector.swift
+++ Navigation/LoginInspector.swift
@@ -13,6 +13,34 @@ class LoginInspector: LoginViewControllerDelegate {
     
     func checkInputData(login: String, password: String) -> String {
         let result = LoginChecker.shared.check(log: login, pass: password)
+        createUser(log: login, pass: password)
         return result
+    }
+    
+    func createUser(log: String, pass: String ) {
+        Auth.auth().createUser(withEmail: log, password: pass) { authResult, error in
+            
+            guard let user = authResult?.user, error == nil else {
+                print(error.debugDescription)
+                return
+            }
+            print("\(user.email!) created")
+        }
+    }
+    
+    func signIn(log: String, pass: String) {
+        Auth.auth().signIn(withEmail: log, password: pass) { authResult, error in
+            
+            if authResult?.user == nil {
+                print("fail, try to create user")
+                //self.createUser(log: log, pass: pass)
+            } else {
+                print("success")
+            }
+            
+            //            print(authResult)
+            print("-------------")
+            print(error.debugDescription)
+        }
     }
 }
