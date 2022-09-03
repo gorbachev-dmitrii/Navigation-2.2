95c9bc775aa24cbf461052aec84254cd57f081ea
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2022-09-03 21:29:18 +0300

    created post objectModel

---
+++ Navigation/CoreData/CoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.swift
+//  Navigation
+//
+//  Created by Dima Gorbachev on 02.09.2022.
+//  Copyright © 2022 Artem Novichkov. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataManager {
+    
+}
