99992781f7d70c802c3444d2c614bc9daaa6c1b0
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2022-10-24 21:39:57 +0300

    added scheduled notifications

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        LocalNotificationsService.shared.registeForLatestUpdatesIfPossible()
+        LocalNotificationsService.shared.scheduleNotification()
         return true
     }
 
---
+++ Navigation/Managers/LocalNotificationsService.swift
@@ -0,0 +1,41 @@
+//
+//  LocalNotificationsService.swift
+//  Navigation
+//
+//  Created by Dima Gorbachev on 24.10.2022.
+//  Copyright © 2022 Artem Novichkov. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+final class LocalNotificationsService {
+    
+    static var shared = LocalNotificationsService()
+    private let center = UNUserNotificationCenter.current()
+
+    func registeForLatestUpdatesIfPossible() {
+        center.requestAuthorization(options: [.badge, .provisional, .sound]) { granted, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.body = "Посмотрите последние обновления"
+        let trigger = UNCalendarNotificationTrigger(dateMatching: createSchedule(), repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    private func createSchedule() -> DateComponents {
+        var dateComponents = DateComponents()
+        dateComponents.hour = 19
+        dateComponents.minute = 0
+        dateComponents.second = 0
+        dateComponents.timeZone = .current
+        return dateComponents
+    }
+}
