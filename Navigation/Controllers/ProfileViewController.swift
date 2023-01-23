4722f3f2cafdcf12980fc2802e0f9d1997750d6c
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2023-01-24 01:27:33 +0300

    refactored ProfileVC, added second section

--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -55,27 +55,27 @@ class ProfileViewController: UIViewController {
     //        self.profileViewModel = viewModel
     //        super.init(nibName: nil, bundle: nil)
     //    }
-//    init(viewModel: ProfileViewModel) {
-//        self.profileViewModel = viewModel
-//        super.init(nibName: nil, bundle: nil)
-//    }
+    //    init(viewModel: ProfileViewModel) {
+    //        self.profileViewModel = viewModel
+    //        super.init(nibName: nil, bundle: nil)
+    //    }
     
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+    //    required init?(coder: NSCoder) {
+    //        fatalError("init(coder:) has not been implemented")
+    //    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
- //       let user = profileViewModel.createUser()
-//        profileHeader.fullNameLabel.text = user.name
-//        profileHeader.statusLabel.text = user.status
-//        profileHeader.avatarImageView.image = UIImage(named: user.avatar)
+        //       let user = profileViewModel.createUser()
+        //        profileHeader.fullNameLabel.text = user.name
+        //        profileHeader.statusLabel.text = user.status
+        //        profileHeader.avatarImageView.image = UIImage(named: user.avatar)
         
-        #if DEBUG
+#if DEBUG
         view.backgroundColor = UIColor.createColor(lightMode: .black, darkMode: .white)
-        #elseif RELEASE
+#elseif RELEASE
         view.backgroundColor = .green
-        #endif
+#endif
         
         profileHeader.statusTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
         profileHeader.addSubview(blurView)
@@ -99,14 +99,14 @@ class ProfileViewController: UIViewController {
                                 delay: 0,
                                 options: [],
                                 animations: {
-                                    UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.3 / 0.8, animations: {
-                                        self.cancelButton.alpha = 0
-                                    })
-                                    UIView.addKeyframe(withRelativeStartTime: 0.5 / 0.8, relativeDuration: 0.5 / 0.8, animations: {
-                                        self.blurView.alpha = 0
-                                        self.profileHeader.avatarImageView.transform = CGAffineTransform.identity
-                                    })
-                                },
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.3 / 0.8, animations: {
+                self.cancelButton.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5 / 0.8, relativeDuration: 0.5 / 0.8, animations: {
+                self.blurView.alpha = 0
+                self.profileHeader.avatarImageView.transform = CGAffineTransform.identity
+            })
+        },
                                 completion: nil)
     }
     
@@ -115,19 +115,19 @@ class ProfileViewController: UIViewController {
                                 delay: 0,
                                 options: [],
                                 animations: {
-                                    UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5 / 0.8, animations: {
-                                        self.blurView.alpha = 0.75
-                                        // смещаем frame по x и y
-                                        let positionTransform = CGAffineTransform(translationX: (self.view.frame.width / 2) - (self.profileHeader.avatarImageView.frame.width / 2) - 16,y: (self.view.frame.height / 2) - self.profileHeader.avatarImageView.frame.height)
-                                        let x = self.view.frame.width / self.profileHeader.avatarImageView.frame.width
-                                        let sizeTransform = CGAffineTransform(scaleX: x, y: x)
-                                        let transform = sizeTransform.concatenating(positionTransform)
-                                        self.profileHeader.avatarImageView.transform = transform
-                                    })
-                                    UIView.addKeyframe(withRelativeStartTime: 0.5 / 0.8, relativeDuration: 0.3 / 0.8, animations: {
-                                        self.cancelButton.alpha = 1
-                                    })
-                                },
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5 / 0.8, animations: {
+                self.blurView.alpha = 0.75
+                // смещаем frame по x и y
+                let positionTransform = CGAffineTransform(translationX: (self.view.frame.width / 2) - (self.profileHeader.avatarImageView.frame.width / 2) - 16,y: (self.view.frame.height / 2) - self.profileHeader.avatarImageView.frame.height)
+                let x = self.view.frame.width / self.profileHeader.avatarImageView.frame.width
+                let sizeTransform = CGAffineTransform(scaleX: x, y: x)
+                let transform = sizeTransform.concatenating(positionTransform)
+                self.profileHeader.avatarImageView.transform = transform
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.5 / 0.8, relativeDuration: 0.3 / 0.8, animations: {
+                self.cancelButton.alpha = 1
+            })
+        },
                                 completion: nil)}
     
     @objc func statusTextChanged(_ textField: UITextField) {
@@ -156,11 +156,10 @@ class ProfileViewController: UIViewController {
 extension ProfileViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         if section == 0 {
-            return 0
-        } else {
             return 220
+        } else {
+            return 40
         }
-        
     }
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return .zero
@@ -169,14 +168,18 @@ extension ProfileViewController: UITableViewDelegate {
 // MARK: UITableViewDataSource
 extension ProfileViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 0
-        } else {
+        switch section {
+        case 0:
+            return 1
+        case 1:
             return posts.count
+        default:
+            return 0
         }
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
+        if indexPath.section == 0 {
             let cell: PhotosTableViewCell = tableView.dequeueReusableCell(withIdentifier: String(describing: PhotosTableViewCell.self), for: indexPath) as! PhotosTableViewCell
             cell.selectionStyle = .default
             return cell
@@ -190,7 +193,7 @@ extension ProfileViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard indexPath.row == 0 else { return }
-//        profileViewModel.onTapShowNextModule()
+        //        profileViewModel.onTapShowNextModule()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -198,10 +201,18 @@ extension ProfileViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
         if section == 0 {
-            return nil
-        } else {
             return profileHeader
+        } else {
+            return nil
+        }
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section:Int) -> String? {
+        switch section {
+        case 0: return "1 section"
+        case 1: return "sectionTitle".localized
+        default: return ""
         }
     }
 }
